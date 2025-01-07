bf1489b309554ae420e1fdadb689d0c5f51a35a9
Arnas Puskunigis <arnaspusk204@gmail.com>
2025-01-07 12:54:02 +0000

    added terrain

--- OpenGL-CW2/main.cpp
+++ OpenGL-CW2/main.cpp
@@ -2,6 +2,8 @@
 #include <glfw3.h>
 #include <stb_image.h>
 
+#include "FastNoiseLite.h"
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -25,34 +27,37 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 //Keyboard Input
 void processInput(GLFWwindow* window);
 
-
+//Tank
 vec3 tankPosition = vec3(0.0f, -1.2f, -2.0f);
 float tankSpeed = 0.05f;
 float tankRotationAngle = 0.0f;
 
+//Crate 1
 vec3 cratePosition = vec3(2.0f, -2.0f, -3.0f);
 float crateRotationAngle = 0.0f;
 
+//Crate 2
 vec3 cratePosition2 = vec3(-2.0f, -2.0f, -3.0f);
 float crateSpeed = 0.005f;
 bool moveUp = true;
 
-//int renderDistance = 128;
-//int mapSize = renderDistance * renderDistance;
-//
-//const int squaresRow = renderDistance - 1;
-//const int trianglesPerSquare = 2;
-//const int triangleGrid = squaresRow * squaresRow * trianglesPerSquare;
+//Procedural generation
+const int renderDistance = 128;
+const int mapSize = renderDistance * renderDistance;
 
-// camera
+const int squaresRow = renderDistance - 1;
+const int trianglesPerSquare = 2;
+const int triangleGrid = squaresRow * squaresRow * trianglesPerSquare;
+
+//Camera
 bool cameraMovementActive = false;
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 float lastX = 800 / 2.0f;
 float lastY = 800 / 2.0f;
 bool firstMouse = true;
 
-// timing
-float deltaTime = 0.0f;	// time between current frame and last frame
+//Timing
+float deltaTime = 0.0f;	//Time between current frame and last frame
 float lastFrame = 0.0f;
 
 
@@ -95,61 +100,180 @@ int main()
     Shader shaderProgram("Shaders/vertex.vert", "Shaders/fragment.frag");
     //==========================
 
+    //Compile shaders into a program using a prewritten header file from : "https://learnopengl.com/code_viewer_gh.php?code=includes/learnopengl/shader.h"
+    Shader terrainShader("Shaders/terrain.vert", "Shaders/terrain.frag");
+    //==========================
 
     // Cube vertices with texture coordinates
     float cubeVertices[] = {
         // Positions          // Texture Coordinates
-        // Front face
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+       -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+       -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
 
-        // Back face
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+       -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+       -0.5f,  0.5f, -0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
 
-         // Left face
-         -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-         -0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         -0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+       -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+       -0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+       -0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+       -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
 
-         // Right face
-          0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-          0.5f,  0.5f, -0.5f,  1.0f, 0.0f,
-          0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-          0.5f, -0.5f,  0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, 1.0f,
 
-          // Top face
-          -0.5f,  0.5f, -0.5f,  0.0f, 0.0f,
-          -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-           0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-           0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+       -0.5f,  0.5f, -0.5f,  0.0f, 0.0f,
+       -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
 
-           // Bottom face
-           -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-            0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-            0.5f, -0.5f,  0.5f,  1.0f, 1.0f,
-           -0.5f, -0.5f,  0.5f,  0.0f, 1.0f
+       -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 1.0f,
+       -0.5f, -0.5f,  0.5f,  0.0f, 1.0f
     };
 
     // Cube indices (for optimization)
     unsigned int cubeIndices[] = {
-        0, 1, 2, 2, 3, 0,  // Front face
-        4, 5, 6, 6, 7, 4,  // Back face
-        8, 9, 10, 10, 11, 8,  // Left face
-        12, 13, 14, 14, 15, 12,  // Right face
-        16, 17, 18, 18, 19, 16,  // Top face
-        20, 21, 22, 22, 23, 20   // Bottom face
+        0, 1, 2, 2, 3, 0,
+        4, 5, 6, 6, 7, 4,
+        8, 9, 10, 10, 11, 8,
+        12, 13, 14, 14, 15, 12,
+        16, 17, 18, 18, 19, 16,
+        20, 21, 22, 22, 23, 20
     };
 
-    //Create and generate texture object for the plane
+    //Terrain ====
+    FastNoiseLite TerrainNoise;
+    //Settings
+    TerrainNoise.SetNoiseType(FastNoiseLite::NoiseType_Perlin);
+    TerrainNoise.SetFrequency(0.02f);
+    //Randomisation
+    int terrainSeed = rand() % 100;
+    TerrainNoise.SetSeed(terrainSeed);
+
+    //Biomes
+    FastNoiseLite BiomeNoise;
+    //Settings
+    BiomeNoise.SetNoiseType(FastNoiseLite::NoiseType_Cellular);
+    BiomeNoise.SetFrequency(0.02f);
+    //Randomisation
+    int biomeSeed = rand() % 100;
+    BiomeNoise.SetSeed(biomeSeed);
+
+    //Terrain
+    GLfloat terrainVertices[mapSize][6];
+    float drawingStartPosition = 1.0f;
+    float columnVerticesOffset = drawingStartPosition;
+    float rowVerticesOffset = drawingStartPosition;
+
+    int rowIndex = 0;
+
+    //Biome colours and terrain height
+    int i = 0;
+    for (int y = 0; y < renderDistance; y++) {
+        for (int x = 0; x < renderDistance; x++) {
+            terrainVertices[i][1] = TerrainNoise.GetNoise((float)x, (float)y);
+
+            float biomeValue = BiomeNoise.GetNoise((float)x, (float)y);
+            if (biomeValue <= -0.75f) { 
+                //Swamp
+                terrainVertices[i][3] = 0.0f;
+                terrainVertices[i][4] = 0.4f;
+                terrainVertices[i][5] = 0.0f;
+            }
+            else 
+            {
+                //Desert
+                terrainVertices[i][3] = 0.9f;
+                terrainVertices[i][4] = 0.9f;
+                terrainVertices[i][5] = 0.1f;
+            }
+
+            i++;
+        }
+    }
+
+    //Set biome colours based on Y values, and terrain verices set up
+    for (int i = 0; i < mapSize; i++) {
+        //vertices
+        terrainVertices[i][0] = columnVerticesOffset;
+        terrainVertices[i][2] = rowVerticesOffset;
+
+        //Rocks
+        if (terrainVertices[i][1] >= (4.0f / 8.0f)) {
+            terrainVertices[i][3] = 0.2f;
+            terrainVertices[i][4] = 0.2f;
+            terrainVertices[i][5] = 0.2f;
+        }
+        //Planes
+        else if (terrainVertices[i][1] >= (1.0f / 8.0f)) {
+            terrainVertices[i][3] = 0.2f;
+            terrainVertices[i][4] = 1.0f;
+            terrainVertices[i][5] = 0.2f;
+        }
+
+        columnVerticesOffset -= 0.0625f;
+        rowIndex++;
+        if (rowIndex == renderDistance) {
+            rowIndex = 0;
+            columnVerticesOffset = drawingStartPosition;
+            rowVerticesOffset -= 0.0625f;
+        }
+    }
+    
+    //Set up the terrain indices
+    GLuint terrainIndices[triangleGrid][3];
+    int columnIndicesOffset = 0;
+    int rowIndicesOffset = 0;
+    rowIndex = 0;
+
+    for (int i = 0; i < triangleGrid - 1; i += 2) {
+        terrainIndices[i][0] = columnIndicesOffset + rowIndicesOffset;
+        terrainIndices[i][2] = renderDistance + columnIndicesOffset + rowIndicesOffset;
+        terrainIndices[i][1] = 1 + columnIndicesOffset + rowIndicesOffset;
+        terrainIndices[i + 1][0] = 1 + columnIndicesOffset + rowIndicesOffset;
+        terrainIndices[i + 1][2] = renderDistance + columnIndicesOffset + rowIndicesOffset;
+        terrainIndices[i + 1][1] = 1 + renderDistance + columnIndicesOffset + rowIndicesOffset;
+        columnIndicesOffset += 1;
+        rowIndex++;
+        if (rowIndex == squaresRow) {
+            rowIndex = 0;
+            columnIndicesOffset = 0;
+            rowIndicesOffset += renderDistance;
+        }
+    }
+
+    unsigned int terrainVAO, terrainVBO, terrainEBO;
+    glGenVertexArrays(1, &terrainVAO);
+    glGenBuffers(1, &terrainVBO);
+    glGenBuffers(1, &terrainEBO);
+
+    glBindVertexArray(terrainVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, terrainVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(terrainVertices), terrainVertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, terrainEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(terrainIndices), terrainIndices, GL_STATIC_DRAW);
+
+    //Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    //Colour attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glBindVertexArray(0);
+
+    //Create and generate texture object for the Signature Cube ====
     unsigned int signatureTexture;
     glGenTextures(1, &signatureTexture);
-    //Cofigure the plane texture settings
     glBindTexture(GL_TEXTURE_2D, signatureTexture);
     //Load the signature.jpg texture and generate the mipmaps
     int width, height, nrChannels;
@@ -162,7 +286,7 @@ int main()
     //If this failed, ouput an error message
     else
     {
-        std::cout << "Could not load the texture for the plane" << std::endl;
+        std::cout << "Could not load the texture for the Signature Cube" << std::endl;
     }
     //Free memory
     stbi_image_free(data);
@@ -231,38 +355,55 @@ int main()
         glm::mat4 projection = glm::perspective(glm::radians(90.0f), (float)800 / (float)800, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
 
-        // Cube rendering
+        //Signature Cube rendering ====
         shaderProgram.use();
 
-        // Set the projection and the view matrices for the cube
+        //Set the projection and the view matrices for the cube
         shaderProgram.setMat4("view", view);
         shaderProgram.setMat4("projection", projection);
 
-        // Set the model matrix for the cube
+        //Set the model matrix for the cube
         glm::mat4 cubeModel = glm::mat4(1.0f);
-        cubeModel = glm::translate(cubeModel, glm::vec3(0.0f, -4.0f, 0.0f)); // Position the cube below the ground
+        cubeModel = glm::translate(cubeModel, glm::vec3(0.0f, -6.0f, 0.0f)); // Position the cube below the ground
         cubeModel = glm::scale(cubeModel, glm::vec3(2.0f, 2.0f, 2.0f)); // Scale the cube
         shaderProgram.setMat4("model", cubeModel);
 
-        // Bind the texture for the cube
+        //Bind the texture for the cube
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, signatureTexture);
 
-        // Bind the VAO and draw the cube
+        //Bind the VAO and draw the cube
         glBindVertexArray(cubeVAO);
         glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
+        //Terrain rendering ====
+        terrainShader.use();
+        //Set the projection and the view matrices for the cube
+        terrainShader.setMat4("view", view);
+        terrainShader.setMat4("projection", projection);
+
+        //Set the model matrix for the cube
+        glm::mat4 terrainModel = glm::mat4(1.0f);
+        terrainModel = glm::translate(terrainModel, glm::vec3(7.0f, -1.0f, 7.0f)); //Position the terrain near and under the tank
+        terrainModel = glm::scale(terrainModel, glm::vec3(5.0f, 5.0f, 5.0f)); //Scale the terrain
+        terrainShader.setMat4("model", terrainModel);
+
+        //Bind the VAO and draw the terrain
+        glBindVertexArray(terrainVAO);
+        glDrawElements(GL_TRIANGLES, mapSize*32, GL_UNSIGNED_INT, 0);
+
         //Tank model ====
-        // Set the projection and the view matrices for the tank
+        shaderProgram.use();
+        //Set the projection and the view matrices for the tank
         shaderProgram.setMat4("projection", projection);
         shaderProgram.setMat4("view", view);
 
-        // Set model matrix for the tank
+        //Set model matrix for the tank
         glm::mat4 tankModel = glm::mat4(1.0f);
         tankModel = glm::translate(tankModel, tankPosition);
         tankModel = glm::rotate(tankModel, glm::radians(tankRotationAngle), glm::vec3(0.0f, 1.0f, 0.0f)); // Rotate the tank
         shaderProgram.setMat4("model", tankModel);
-        // Draw the tank model
+        //Draw the tank model
         tank.Draw(shaderProgram);
 
         shaderProgram.setMat4("projection", projection);
@@ -273,7 +414,7 @@ int main()
         crateModel = glm::rotate(crateModel, glm::radians(crateRotationAngle), glm::vec3(0.0f, 1.0f, 0.0f)); // Rotate the crate
         crateModel = glm::scale(crateModel, glm::vec3(0.050f, 0.050f, 0.050f)); // Scale the crate
         shaderProgram.setMat4("model", crateModel);
-        // Draw the crate model
+        //Draw the crate model
         crate.Draw(shaderProgram);
 
         shaderProgram.setMat4("projection", projection);
@@ -284,7 +425,7 @@ int main()
         crateModel2 = glm::rotate(crateModel2, glm::radians(0.0f), glm::vec3(0.0f, 1.0f, 0.0f)); // Rotate the crate
         crateModel2 = glm::scale(crateModel2, glm::vec3(0.050f, 0.050f, 0.050f)); // Scale the crate
         shaderProgram.setMat4("model", crateModel2);
-        // Draw the crate model
+        //Draw the other crate model
         crate2.Draw(shaderProgram);
 
         //Draw the screen and process input
@@ -296,6 +437,7 @@ int main()
     glfwTerminate();
     //Delete the shader program
     glDeleteProgram(shaderProgram.ID);
+    glDeleteProgram(terrainShader.ID);
     //Exit code
     return 0;
 }
--- OpenGL-CW2/shaders/terrain.vert
+++ OpenGL-CW2/shaders/terrain.vert
@@ -1,9 +1,8 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aNormal;
-layout (location = 2) in vec2 aTexCoords;
+layout (location = 1) in vec3 aColour;
 
-out vec2 TexCoords;
+out vec3 outColour;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -12,5 +11,5 @@ uniform mat4 projection;
 void main()
 {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
-    TexCoords = aTexCoords;    
+    outColour = aColour;
 }
